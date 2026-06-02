06601fa0ae2e57ba9cf78b0907496365090c893b
mfebriansyaaah <mfebriansyaaah@users.noreply.github.com>
2026-06-03 01:58:42 +0700

    deploy: fix server.js socket initialization crash on Passenger

--- server.js
+++ server.js
@@ -3,10 +3,7 @@ const { parse } = require('url');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
-const hostname = 'localhost';
-const port = process.env.PORT || 3000;
-
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -19,8 +16,8 @@ app.prepare().then(() => {
       res.statusCode = 500;
       res.end('internal server error');
     }
-  }).listen(port, (err) => {
+  }).listen(process.env.PORT || 3000, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://${hostname}:${port}`);
+    console.log('> Ready');
   });
 });
